3da55592e75e0103eabdf0ebedd44082b51365a9
Dmitry Pripa <dmitrypripa@gmail.com>
2023-12-31 20:07:19 +0200

    release 1.0.0

--- inc/StatusNotice.php
+++ inc/StatusNotice.php
@@ -4,14 +4,6 @@ namespace WPDevAssist;
 defined( 'ABSPATH' ) || exit;
 
 class StatusNotice {
-	public function __construct() {
-		if ( ! static::is_destination_page() ) {
-			return;
-		}
-
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
-	}
-
 	public static function render( string $message, string $level = 'default' ): void {
 		if ( ! static::is_destination_page() ) {
 			return;
@@ -21,11 +13,6 @@ class StatusNotice {
 		$content .= $message;
 
 		Plugin\Notice::render( $content, "$level wpda-status" );
-	}
-
-	public function enqueue_assets(): void {
-		Plugin\Asset::enqueue_style( 'status-notice' );
-		Plugin\Asset::enqueue_script( 'status-notice' );
 	}
 
 	protected static function is_destination_page(): bool {
